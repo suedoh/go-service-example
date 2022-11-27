7f2d17bb245922ff78120d7d183fdab56908208f
suedoh <suedoh@proton.me>
2022-11-27 14:34:56 -0500

    created a basic service

---
+++ service.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+)
+
+type Service interface{
+	GetCatFact(context.Context)(*CatFact, error)
+}
+
+type CatFactService struct {
+	url string
+}
+
+func (s *CatFactService) GetCatFact(ctx context.Context) (*CatFact, error) {
+	resp, err := http.Get(s.url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	fact := &CatFact{}
+	if err := json.NewDecoder(resp.Body).Decode(fact); err != nil {
+		return nil, err
+	}
+
+	return fact, err
+}
+
+func NewCatFactService(url string) (Service) {
+	return &CatFactService{
+		url: url,
+	}
+}
+
